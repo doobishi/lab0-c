17f70a37ffd75f192715651c33b3cd9c3ac5de39
doobi_rstn_computer <sudo0apt0install@gmail.com>
2026-03-06 16:40:37 +0800

    Implement tail removal from queue
    - Verified head pointer and checked for empty queue states.
    - Used snprintf for safe string copying into the provided buffer.
    - Properly returned the element_t pointer for external management.
    
    Change-Id: Ic24fb375b1e31bc4640c7f4f993e32fb8dfb013a

--- queue.c
+++ queue.c
@@ -83,7 +83,18 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *target = list_entry(head->next, element_t, list);
+
+    if (sp && target->value) {
+        snprintf(sp, bufsize, "%s", target->value);
+    }
+
+    list_del(&target->list);
+
+    return target;
 }
 
 /* Remove an element from tail of queue */
